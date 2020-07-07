a7b4823802d5878c760aa5300b14aa168b80f35c
Driss Boumlik <drissboumlik.work@gmail.com>
2020-07-07 17:11:14 +0100

    Add encours module

---
+++ app/Models/EnCours.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EnCours extends Model
+{
+    //
+
+    protected $fillable = [
+        'agent_traitant',
+        'region',
+        'Prestataire',
+        'nome_tech',
+        'prenom_tech',
+        'date',
+        'creneaux',
+        'type',
+        'client',
+        'as',
+        'code_postal',
+        'ville',
+        'voie',
+        'rue',
+        'numero_abo',
+        'nom_abo',
+        'report_multiple',
+        'cause_report',
+        'statut_report',
+        'accord_region',
+    ];
+}
---
+++ database/migrations/2020_07_07_152102_create_en_cours_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnCoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('en_cours', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('agent_traitant', 100)->nullable();
+            $table->string('region', 100)->nullable();
+            $table->string('Prestataire', 100)->nullable();
+            $table->string('nome_tech', 100)->nullable();
+            $table->string('prenom_tech', 100)->nullable();
+            $table->date('date')->nullable();
+            $table->string('creneaux', 100)->nullable();
+            $table->string('type', 100)->nullable();
+            $table->string('client', 100)->nullable();
+            $table->string('as', 100)->nullable();
+            $table->string('code_postal', 100)->nullable();
+            $table->string('ville', 100)->nullable();
+            $table->string('voie', 100)->nullable();
+            $table->string('rue')->nullable();
+            $table->string('numero_abo', 100)->nullable();
+            $table->string('nom_abo', 100)->nullable();
+            $table->string('report_multiple', 100)->nullable();
+            $table->string('cause_report', 100)->nullable();
+            $table->string('statut_report', 100)->nullable();
+            $table->string('accord_region', 100)->nullable();
+            $table->boolean('isNotReady')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('encours');
+    }
+}
