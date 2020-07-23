b37b243ba7f95db7359589193487c36c50078e73
Arway <arfa.abderrahman@gmail.com>
2020-07-23 10:15:57 +0100

    change the input format and add required to the inputs

--- resources/views/b2bsfr/new.blade.php
+++ resources/views/b2bsfr/new.blade.php
@@ -82,7 +82,7 @@
                                                      'SO'    => 'SE',
                                                      'CORSE' => 'CORSE'
                                                      ],
-                                        $region ?? '', ["class" => "form-control", "id" => "region", "placeholder" => "choisissez une region"] ) !!}
+                                        $region ?? '', ["class" => "form-control", "id" => "region", "placeholder" => "choisissez une region", 'required'] ) !!}
 
                     </div>
                 </div>
@@ -90,15 +90,15 @@
                 <div class="row mb-2">
                     <div class="col-md-4">
                         <label for="numero_intervention">Numéro d'intervention </label>
-                        <input type="text" class="form-control" id="numero_intervention" name="numero_intervention" value="{{old('numero_intervention')}}" placeholder="Numéro d'intervention">
+                        <input type="text" class="form-control" id="numero_intervention" name="numero_intervention" value="{{old('numero_intervention')}}" placeholder="Numéro d'intervention" required>
                     </div>
                     <div class="col-md-4">
                         <label for="cdp">CDP </label>
-                        <input type="text" class="form-control" id="cdp" name="cdp" value="{{old('cdp')}}" placeholder="CDP">
+                        <input type="text" class="form-control" id="cdp" name="cdp" value="{{old('cdp')}}" placeholder="CDP" required>
                     </div>
                     <div class="col-md-4">
                         <label for="num_cdp">Num CDP </label>
-                        <input type="text" class="form-control" id="num_cdp" name="num_cdp" value="{{old('num_cdp')}}" placeholder="Num CDP">
+                        <input type="text" class="form-control" id="num_cdp" name="num_cdp" value="{{old('num_cdp')}}" placeholder="Num CDP" required>
                     </div>
                 </div>
 
@@ -110,17 +110,17 @@
                                                                'IMES STIT' => 'IMES STIT',
                                                                'IMES Extranet' => 'IMES Extranet'
                                                                ],
-                                        $type_intervention ?? '', ["class" => "form-control", "id" => "type_intervention", "placeholder" => "choisissez une type d'intervention"] ) !!}
+                                        $type_intervention ?? '', ["class" => "form-control", "id" => "type_intervention", "placeholder" => "choisissez une type d'intervention", 'required'] ) !!}
                     </div>
                     <div class="col-md-6">
                         <label for="Sous_type_Inter">Sous-type intervention</label>
-                        <input type="text" class="form-control" id="Sous_type_Inter" name="Sous_type_Inter" value="{{old('Sous_type_Inter')}}" placeholder="Sous-type intervention">
+                        <input type="text" class="form-control" id="Sous_type_Inter" name="Sous_type_Inter" value="{{old('Sous_type_Inter')}}" placeholder="Sous-type intervention" required>
                     </div>
                 </div>
                 <div class="row mb-2">
                     <div class="col-md-4">
                         <label for="client">Client</label>
-                        <input type="text" class="form-control" id="client" name="client" value="{{old('client')}}" placeholder="Client">
+                        <input type="text" class="form-control" id="client" name="client" value="{{old('client')}}" placeholder="Client" required>
                     </div>
                     <div class="col-md-4">
                         <label for="cp">CP</label>
@@ -147,36 +147,36 @@
                                                  '94' => '94',
                                                  '95' => '95',
                                                ],
-                                        $cp ?? '', ["class" => "form-control", "id" => "cp", "placeholder" => "choisissez un code postal"] ) !!}
+                                        $cp ?? '', ["class" => "form-control", "id" => "cp", "placeholder" => "choisissez un code postal", 'required'] ) !!}
                     </div>
                     <div class="col-md-4">
                         <label for="ville">Ville</label>
-                        <input type="text" class="form-control" id="ville" name="ville" value="{{old('ville')}}" placeholder="Ville">
+                        <input type="text" class="form-control" id="ville" name="ville" value="{{old('ville')}}" placeholder="Ville" required>
                     </div>
                 </div>
 
                 <div class="row mb-2">
                     <div class="col-md-3">
                         <label for="date_reception">Date Réception Ticket</label>
-                        <input type="datetime-local" class="form-control" name="date_reception" id="date_reception" value="{{'date_reception'}}">
+                        <input type="datetime-local" class="form-control" name="date_reception" id="date_reception" value="{{'date_reception'}}" required>
                     </div>
                     <div class="col-md-3">
                         <label for="date_planification">Date planification</label>
-                        <input type="datetime-local" class="form-control" name="date_planification" id="date_planification" value="{{'date_planification'}}">
+                        <input type="datetime-local" class="form-control" name="date_planification" id="date_planification" value="{{'date_planification'}}" required>
                     </div>
                     <div class="col-md-3">
                         <label for="report">Report</label>
-                        <input type="datetime-local" class="form-control" name="report" id="report" value="{{'report'}}">
+                        <input type="datetime-local" class="form-control" name="report" id="report" value="{{'report'}}" required>
                     </div>
                     <div class="col-md-3">
                         <label for="motif_report">Motif Report</label>
-                        <input type="text" class="form-control" name="motif_report" id="motif_report" value="{{old('motif_report')}}">
+                        <input type="text" class="form-control" name="motif_report" id="motif_report" value="{{old('motif_report')}}" required>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="commentaire_report">Commentaire Report</label>
-                    <textarea class="form-control" id="commentaire_report" name="commentaire_report" rows="4" placeholder="Commentaire...">{{ old('commentaire_report') }}</textarea>
+                    <textarea class="form-control" id="commentaire_report" name="commentaire_report" rows="4" placeholder="Commentaire..." required>{{ old('commentaire_report') }}</textarea>
                 </div>
 
                 <div class="row mb-2">
@@ -185,7 +185,7 @@
                         {!! Form::select('statut_finale', ['ok' => 'ok',
                                                            'ko' => 'ko',
                                                            ],
-                                        $statut_finale ?? '', ["class" => "form-control", "id" => "statut_finale", "placeholder" => "choissisez un statut"] ) !!}
+                                        $statut_finale ?? '', ["class" => "form-control", "id" => "statut_finale", "placeholder" => "choissisez un statut", 'required'] ) !!}
                     </div>
                     <div class="col-md-6 motif_handler d-none">
                         <label for="motif_ko">Motif KO</label>
@@ -202,19 +202,20 @@
                 <div class="row mb-2">
                     <div class="col-md-3">
                         <label for="nom_tech">Nom Tech</label>
-                        <input type="text" class="form-control" id="nom_tech" name="nom_tech" value="{{old('nom_tech')}}" placeholder="Nom Tech">
+                        <input type="text" class="form-control" id="nom_tech" name="nom_tech" value="{{old('nom_tech')}}" placeholder="Nom Tech" required>
                     </div>
                     <div class="col-md-3">
                         <label for="prenom_tech">Prénom Tech</label>
-                        <input type="text" class="form-control" id="prenom_tech" name="prenom_tech" value="{{old('prenom_tech')}}" placeholder="Prénom Tech">
+                        <input type="text" class="form-control" id="prenom_tech" name="prenom_tech" value="{{old('prenom_tech')}}" placeholder="Prénom Tech" required>
                     </div>
                     <div class="col-md-3">
                         <label for="num_tel">Num Tél</label>
-                        <input type="tel" class="form-control" id="num_tel" name="num_tel" value="{{old('num_tel')}}" placeholder="Num Tél">
+                        <input type="tel" class="form-control" id="num_tel" name="num_tel" value="{{old('num_tel')}}"
+                               placeholder="Num Tél " pattern="[0-9]{10}" required>
                     </div>
                     <div class="col-md-3">
                         <label for="adresse_mail">Adresse Mail</label>
-                        <input type="tel" class="form-control" id="adresse_mail" name="adresse_mail" value="{{old('adresse_mail')}}" placeholder="Adresse Mail">
+                        <input type="email" class="form-control" id="adresse_mail" name="adresse_mail" value="{{old('adresse_mail')}}" placeholder="Adresse Mail" required>
                     </div>
                 </div>
 
@@ -224,7 +225,7 @@
                         {!! Form::select('as_j_1', ['oui' => 'oui',
                                                     'non' => 'non',
                                                     ],
-                                        $as_j_1 ?? '', ["class" => "form-control", "id" => "as_j_1", "placeholder" => ""] ) !!}
+                                        $as_j_1 ?? '', ["class" => "form-control", "id" => "as_j_1", "placeholder" => "", 'required'] ) !!}
                     </div>
                     <div class="col-md-6">
                         <label for="statut_ticket">Statut Ticket</label>
@@ -232,14 +233,14 @@
                                                     'Validé' => 'Validé',
                                                     'Clôturé' => 'Clôturé'
                                                     ],
-                                        $statut_ticket ?? '', ["class" => "form-control", "id" => "statut_ticket", "placeholder" => "choissisez un statut"] ) !!}
+                                        $statut_ticket ?? '', ["class" => "form-control", "id" => "statut_ticket", "placeholder" => "choissisez un statut", 'required'] ) !!}
 
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="commentaire">Commentaire</label>
-                    <textarea class="form-control" id="commentaire" name="commentaire" rows="4" placeholder="Commentaire...">{{ old('commentaire') }}</textarea>
+                    <textarea class="form-control" id="commentaire" name="commentaire" rows="4" placeholder="Commentaire..." required>{{ old('commentaire') }}</textarea>
                 </div>
                 <button class="btn btn-square btn-warning btn-wdth-rc" type="submit" name="submit-tickets">
                             Ajouter le ticket
