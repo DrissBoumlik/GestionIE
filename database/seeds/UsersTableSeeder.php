165094fc0858158aa0a0116dac1d7ecbe76809d7
omar chanane <omarchanane50@gmail.com>
2020-09-01 13:34:22 +0100

    add access to new agents

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -60,17 +60,80 @@ class UsersTableSeeder extends Seeder
             'remember_token' => Str::random(10),
             'role_id' => 4
         ]);
+
+
+        //new agents
         User::create([
-            'firstname' => 'John',
-            'lastname' => 'B2bSfrAdmin',
-            'email' => 'ad.b2bSfr@circet.fr',
+            'firstname' => 'Majdouline',
+            'lastname' => 'ABDELHAKIM',
+            'email' => 'majdouline.abdelhakim@circet.fr',
             'status' => true,
-            'picture' => '/media/avatars/user.png',
-            'gender' => 'male',
+            'picture' => '/media/avatars/female.png',
+            'gender' => 'female',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'remember_token' => Str::random(10),
-            'role_id' => 5
+            'role_id' => 3
+        ]);
+        User::create([
+            'firstname' => 'Hasnaa',
+            'lastname' => 'HARAKATI',
+            'email' => 'hasnaa.harakati@circet.fr',
+            'status' => true,
+            'picture' => '/media/avatars/female.png',
+            'gender' => 'female',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'remember_token' => Str::random(10),
+            'role_id' => 3
+        ]);
+        User::create([
+            'firstname' => 'Yasmine',
+            'lastname' => 'EL-KAMA',
+            'email' => 'yasmine.el-kama@circet.fr',
+            'status' => true,
+            'picture' => '/media/avatars/female.png',
+            'gender' => 'female',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'remember_token' => Str::random(10),
+            'role_id' => 3
+        ]);
+            User::create([
+            'firstname' => 'Chaimaa',
+            'lastname' => 'CHOUHBI',
+            'email' => 'chaimaa.chouhbi@circet.fr',
+            'status' => true,
+            'picture' => '/media/avatars/female.png',
+            'gender' => 'female',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'remember_token' => Str::random(10),
+            'role_id' => 3
+        ]);
+            User::create([
+            'firstname' => 'Oumayma',
+            'lastname' => 'BOUCHACHIA',
+            'email' => 'oumayma.bouchachia@circet.fr',
+            'status' => true,
+            'picture' => '/media/avatars/female.png',
+            'gender' => 'female',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'remember_token' => Str::random(10),
+            'role_id' => 3
+        ]);
+            User::create([
+            'firstname' => 'Jihane',
+            'lastname' => 'KHOUYA',
+            'email' => 'jihane.khouya@circet.fr',
+            'status' => true,
+            'picture' => '/media/avatars/female.png',
+            'gender' => 'female',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'remember_token' => Str::random(10),
+            'role_id' => 3
         ]);
     }
 }
